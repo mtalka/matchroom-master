b07e80909cfe69cf7c45007291089f7ead5731e3
Markus Talka <markus.talka@ecraft.com>
2020-07-31 23:02:22 +0300

    Forcing SSL / https

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+});
+
 // Serve the static files
 app.use(express.static("dist"));
 
